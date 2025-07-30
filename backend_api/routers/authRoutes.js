a40be6cc245894f5370966bb7fdd7969e309d6de
dharmik prime <121918467+Dharv31@users.noreply.github.com>
2025-07-30 19:29:12 +0530

    Revert "first commit"
    This reverts commit 69a61b5c3069cd7e1c9ff11bd63cdfee99bd6ac8.

--- backend_api/routers/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, handleRedirect } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.get('/login', login);
-router.get('/redirect', handleRedirect);
-
-export default router;
--- backend_api/routers/graphRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { authenticateToken } from '../middleware/authMiddleware.js';
-import { getMessages } from '../controllers/graphController.js';
-
-const router = express.Router();
-
-router.get('/messages', authenticateToken, getMessages);
-
-export default router;
